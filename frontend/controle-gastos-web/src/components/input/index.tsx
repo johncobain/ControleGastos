7afa0105d89250a31e4ad36ee3f2e475d14fb68c
Andrey Gomes <andreygdsn@gmail.com>
2026-07-14 17:00:32 -0300

    feat: add input component

---
+++ frontend/controle-gastos-web/src/components/input/index.tsx
@@ -0,0 +1,38 @@
+import type { InputHTMLAttributes } from "react";
+import "./styles.css";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+}
+
+const Input = ({
+  label,
+  error,
+  id,
+  className = "",
+  ...props
+}: InputProps) => {
+  return (
+    <div className="flex-column gap-sm">
+      {label && (
+        <label htmlFor={id} className="input-label">
+          {label}
+        </label>
+      )}
+      <input 
+        id={id}
+        className={`input ${error ? "input-error" : ""} ${className}`}
+        {...props}
+      />
+
+      {error && (
+        <span className="input-error-message">
+          {error}
+        </span>
+      )}
+    </div>
+  );
+}
+
+export default Input;
