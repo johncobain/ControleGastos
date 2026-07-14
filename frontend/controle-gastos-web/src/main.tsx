58367d2b1c4de0979e23e95e0cbbbe5c91575c81
Andrey Gomes <andreygdsn@gmail.com>
2026-07-14 16:24:27 -0300

    styles: update styles

--- frontend/controle-gastos-web/src/main.tsx
+++ frontend/controle-gastos-web/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 import "./styles/variables.css";
-import "./styles/reset.css";
+import "./styles/utilities.css"
 import "./styles/global.css";
 
 createRoot(document.getElementById('root')!).render(
