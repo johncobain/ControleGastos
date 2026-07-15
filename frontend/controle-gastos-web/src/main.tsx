23724c2133cf649a5cc32a26bdc86610a6291465
Andrey Gomes <andreygdsn@gmail.com>
2026-07-15 02:24:32 -0300

    build: add zod

--- frontend/controle-gastos-web/src/main.tsx
+++ frontend/controle-gastos-web/src/main.tsx
@@ -1,5 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import App from './App.tsx'
 
 import "./styles/variables.css";
@@ -7,7 +9,15 @@ import "./styles/utilities.css"
 import "./styles/global.css";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+    <ToastContainer
+      position="bottom-right"
+      autoClose={3000}
+      newestOnTop
+      pauseOnFocusLoss={false}
+      pauseOnHover
+      theme="dark"
+    />
+  </>,
 )
