f34fc4d68c1fa5a1602b776d93f018867e04914c
Andrey Gomes <andreygdsn@gmail.com>
2026-07-15 14:41:42 -0300

    feat: add select suport

--- frontend/controle-gastos-web/src/components/input/index.tsx
+++ frontend/controle-gastos-web/src/components/input/index.tsx
@@ -1,20 +1,30 @@
-import type { InputHTMLAttributes } from "react";
+import type { InputHTMLAttributes, SelectHTMLAttributes } from "react";
 import "./styles.css";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  options?: never;
   error?: string;
+  id: string;
+  className?: string;
+}
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  label?: string;
+  error?: string;
+  options: { value: string; label: string }[];
   id: string;
   className?: string;
 }
 
 const Input = ({
   label,
+  options,
   error,
   id,
   className = "",
   ...props
-}: InputProps) => {
+}: InputProps | SelectProps) => {
   return (
     <div className="flex-column gap-sm">
       {label && (
@@ -22,12 +32,25 @@ const Input = ({
           {label}
         </label>
       )}
-      <input 
-        id={id}
-        className={`input ${error ? "input-error" : ""} ${className}`}
-        {...props}
-      />
-
+      {options ? (
+        <select
+          id={id}
+          className={`input ${error ? "input-error" : ""} ${className}`}
+          {...(props as Omit<SelectProps, "id">)}
+        >
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input 
+          id={id}
+          className={`input ${error ? "input-error" : ""} ${className}`}
+          {...(props as Omit<InputProps, "id">)}
+        />
+      )}
       {error && (
         <span className="input-error-message">
           {error}
