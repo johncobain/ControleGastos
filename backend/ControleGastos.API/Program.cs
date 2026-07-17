28e5c95f2a6b8ca4bd85347c45a876b59926d207
Andrey Gomes <andreygdsn@gmail.com>
2026-07-16 21:03:52 -0300

    buid: configure production database

--- backend/ControleGastos.API/Program.cs
+++ backend/ControleGastos.API/Program.cs
@@ -84,8 +84,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors(CorsPolicyName);
 
